5406f69f00ff56f8c6e3f66a175a61a763db0365
Anirudh Sharma <anirudh.sharma.ars@gmail.com>
2025-09-01 19:15:55 +0530

    dynamic lock trying

--- ReflectionLog/open.py
+++ ReflectionLog/open.py
@@ -1,11 +1,36 @@
 import json
 from lockcommand import unlock_journal
 
+from sentence_transformers import SentenceTransformer,util
+#from lock import tags
+
+data = unlock_journal("3937", infile="memory.enc", saltfile="salt.bin")
+with open("memory.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+
+
+y = input("please tell the passphrase")
+    
+model = SentenceTransformer("all-MiniLM-L6-v2")
+embeddings = model.encode(tags[0])
+
+    
+        
+
+
 
 while True:
+    
     try:
-        passphrase = input("Enter the passphrase")
-        data = unlock_journal(passphrase, infile="memory.enc", saltfile="salt.bin")
+        passphrase = input("What did you talk about in your last entry?? -")
+        cos_sim = util.cos_sim(embeddings, passphrase)
+        if max(cos_sim) >0.75:
+            print("congratulations")
+            
+            data = unlock_journal("3937", infile="memory.enc", saltfile="salt.bin")
+        else:
+            print("Password incorrect.Try again")
 
 # Write it back as plaintext JSON
         with open("memory.json", "w") as f:
